7284d17b9529ba5e3c900c7b8c046b2b16dce1f6
Rekcos <kraftkon@hotmail.com>
2020-02-06 11:00:00 +0100

    Database added

---
+++ Database/GladiatorDatabase/Migrations/20200201161728_Initial.cs
@@ -0,0 +1,189 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GladiatorDatabase.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "BattleResults",
+                columns: table => new
+                {
+                    BattleResultId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    WinnerId = table.Column<int>(nullable: false),
+                    LoserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BattleResults", x => x.BattleResultId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GladiatorKills",
+                columns: table => new
+                {
+                    GladiatorKillsId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    KilledId = table.Column<int>(nullable: false),
+                    BattleId = table.Column<int>(nullable: false),
+                    BattleResultId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GladiatorKills", x => x.GladiatorKillsId);
+                    table.ForeignKey(
+                        name: "FK_GladiatorKills_BattleResults_BattleResultId",
+                        column: x => x.BattleResultId,
+                        principalTable: "BattleResults",
+                        principalColumn: "BattleResultId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lanistas",
+                columns: table => new
+                {
+                    LanistaId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LanistaName = table.Column<string>(nullable: true),
+                    UserId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lanistas", x => x.LanistaId);
+                    table.ForeignKey(
+                        name: "FK_Lanistas_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Gladiators",
+                columns: table => new
+                {
+                    GladiatorId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Wins = table.Column<int>(nullable: false),
+                    Loss = table.Column<int>(nullable: false),
+                    Kills = table.Column<int>(nullable: false),
+                    Alive = table.Column<bool>(nullable: false),
+                    LanistaId = table.Column<int>(nullable: false),
+                    BattleId = table.Column<int>(nullable: false),
+                    LastBattleBattleResultId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Gladiators", x => x.GladiatorId);
+                    table.ForeignKey(
+                        name: "FK_Gladiators_Lanistas_LanistaId",
+                        column: x => x.LanistaId,
+                        principalTable: "Lanistas",
+                        principalColumn: "LanistaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Gladiators_BattleResults_LastBattleBattleResultId",
+                        column: x => x.LastBattleBattleResultId,
+                        principalTable: "BattleResults",
+                        principalColumn: "BattleResultId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Type = table.Column<int>(nullable: false),
+                    LanistaId = table.Column<int>(nullable: true),
+                    GladiatorId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.ItemId);
+                    table.ForeignKey(
+                        name: "FK_Items_Gladiators_GladiatorId",
+                        column: x => x.GladiatorId,
+                        principalTable: "Gladiators",
+                        principalColumn: "GladiatorId",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Items_Lanistas_LanistaId",
+                        column: x => x.LanistaId,
+                        principalTable: "Lanistas",
+                        principalColumn: "LanistaId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GladiatorKills_BattleResultId",
+                table: "GladiatorKills",
+                column: "BattleResultId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gladiators_LanistaId",
+                table: "Gladiators",
+                column: "LanistaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Gladiators_LastBattleBattleResultId",
+                table: "Gladiators",
+                column: "LastBattleBattleResultId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_GladiatorId",
+                table: "Items",
+                column: "GladiatorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_LanistaId",
+                table: "Items",
+                column: "LanistaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Lanistas_UserId",
+                table: "Lanistas",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GladiatorKills");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Gladiators");
+
+            migrationBuilder.DropTable(
+                name: "Lanistas");
+
+            migrationBuilder.DropTable(
+                name: "BattleResults");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ Database/GladiatorDatabase/Program.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace GladiatorDatabase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new EditorContext())
+            {
+                Console.WriteLine("Temp Switch Meny");
+                Console.WriteLine("1: User. 2: Lanistas. 3: Gladiator. 4: Items. 5: BattleResult. 6: GladiatorKills. ");
+                var c = Console.ReadKey();
+
+                switch (c.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("\nUser: ");
+                        Console.WriteLine("1: Create. 2: Read. 3: Update. 4: Destroy. 5: Cancel");                        
+                        var crud = Console.ReadKey();
+                        
+                        Usermanager usermanager = new Usermanager(db);
+                        usermanager.Read();
+                        usermanager.Update();
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine("Lanistas: ");
+                        Console.WriteLine("1: Create. 2: Read. 3: Update. 4: Destroy.");
+                        break;
+                    case ConsoleKey.D3:
+                        Console.WriteLine("Gladiator: ");
+                        Console.WriteLine("1: Create. 2: Read. 3: Update. 4: Destroy.");
+                        break;
+                    case ConsoleKey.D4:
+                        Console.WriteLine("Items: ");
+                        Console.WriteLine("1: Create. 2: Read. 3: Update. 4: Destroy.");
+                        break;
+                    case ConsoleKey.D5:
+                        Console.WriteLine("BattleResult: ");
+                        Console.WriteLine("1: Create. 2: Read. 3: Update. 4: Destroy.");
+                        break;
+                    case ConsoleKey.D6:
+                        Console.WriteLine("GladiatorKills: ");
+                        Console.WriteLine("1: Create. 2: Read. 3: Update. 4: Destroy.");
+                        break;
+                }
+
+                 
+                /*
+                Console.WriteLine("User Name:");
+                var name = Console.ReadLine();
+                
+                var User = new User { UserName = name };
+                db.Users.Add(User);
+                db.SaveChanges();
+
+                Console.WriteLine("Lanista Name:");
+                name = Console.ReadLine();
+                
+                var Lanista = new Lanista { LanistaName = name };
+                db.Lanistas.Add(Lanista);
+                db.SaveChanges();
+
+                var test = from s in db.Lanistas orderby s.LanistaName select s;
+                 
+                foreach (var item in test)
+                {
+                    Console.WriteLine(item.LanistaName);
+                    Console.WriteLine(item.LanistaId);
+                };*/
+                Console.WriteLine("**********");
+                Console.ReadKey();
+
+
+            }
+        }
+    }
+}
+
+public class Usermanager
+{
+    public Usermanager(EditorContext db)
+    {
+         DB = db;
+    }
+
+    EditorContext DB { get; set; }
+    
+    public void Create()
+    {
+        Console.WriteLine("\n*Create User*");
+        Console.WriteLine("User Name: ");
+        var name = Console.ReadLine();
+        var User = new User { UserName = name };
+        DB.Users.Add(User);
+        DB.SaveChanges();
+    }
+    public void Read() 
+    {   
+        Console.WriteLine("\n*Read User*");
+        Console.WriteLine("User Name: ");
+        var SortUsers = from s in DB.Users orderby s.UserName select s;
+        
+        foreach (var item in SortUsers)
+        {
+            Console.WriteLine("User: {0} UserId: {1}", item.UserName, item.UserId);
+        };
+    }
+    public void Update()
+    {
+        Console.WriteLine("\n*Update User*");
+        Console.WriteLine("User Name: ");
+        var name = Console.ReadLine();
+        var dupUserName = DB.Users.Where(s => s.UserName == name).ToList();
+        User user;
+        
+        if ((dupUserName.Count != 1) && (dupUserName.Count > 0)) 
+        { 
+            Console.WriteLine("\nNot Unik User Name.");
+            Console.WriteLine("\nEnter Id: ");
+            var check = int.TryParse(Console.ReadLine(), out int userid);
+            if (check) 
+            {
+
+                user = DB.Users.Find(userid);
+            }
+            else
+            {
+                Console.WriteLine("\nInvalid Id Input!\n");
+            }           
+        }
+        else if (dupUserName.Count == 1)
+        {
+
+            user = dupUserName.Single();
+
+        }
+        else
+        {
+            Console.WriteLine("\nInvalid Name Input!\n");
+            
+        }
+
+
+        Console.WriteLine("\nUser Name: ");
+        var input = Console.ReadLine();
+        user.UserName = input;
+        Console.WriteLine("\nUser Password: ");
+        DB.Users.Find(update).Password = Console.ReadLine();
+        Console.WriteLine("\nUser e-mail: ");
+        DB.Users.Find(update).Email = Console.ReadLine();
+        
+        Console.WriteLine("Confirm [y]: ");
+        var key = Console.ReadKey();
+        if(key.Key == ConsoleKey.Y) 
+        {
+            DB.Users.Update(DB.Users.Find(update));
+            DB.SaveChanges();
+        }
+
+
+    }
+    public void Destroy()
+    {
+        Console.WriteLine("\n*Destroy User*");
+        Console.WriteLine("\nUser Name: ");
+        var username = Console.ReadLine();
+        Console.WriteLine("\nUser ID: ");
+        int.TryParse(Console.ReadLine(), out int userid);
+
+        
+        
+        
+
+        
+    }
+
+}
+
+public class User
+{
+    public int UserId { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string Email { get; set; }
+    public virtual List<Lanista> Lanistas { get; set; }
+     
+}
+
+public class Lanista
+{
+    public int LanistaId { get; set; }
+    public string LanistaName { get; set; }
+
+    public int UserId { get; set; }
+    public virtual User User { get; set; }
+    public virtual List<Item> Items { get; set; }
+    public virtual List<Gladiator> Gladiators { get; set; }
+
+}
+
+public class Gladiator
+{
+    public int GladiatorId { get; set; }
+    public struct Stats
+    {
+       public int Health;
+       public int Stamina;
+       public int Speed;
+       public int Morale;
+       public int Strength;
+       public int Defense;
+    }
+    public int Wins { get; set; }
+    public int Loss { get; set; }
+    public int Kills { get; set; }
+    public bool Alive { get; set; }
+
+    public int LanistaId { get; set; }
+    public virtual Lanista Lanista { get; set; }
+    public int BattleId { get; set; }
+    public virtual BattleResult LastBattle { get; set; }
+    public virtual List<Item> Equipment { get; set; }
+
+
+}
+
+public class Item
+{  
+    public int ItemId { get; set; }
+    public int Type { get; set; }
+
+    public struct Stats
+    {
+       public int Health;
+       public int Stamina;
+       public int Speed;
+       public int Morale;
+       public int Strength;
+       public int Defense;
+    }
+    public virtual Lanista Lanista { get; set; }
+    public virtual Gladiator Gladiator { get; set; }
+     
+}
+
+
+public class GladiatorKills
+{
+    public int GladiatorKillsId { get; set; }
+    public int KilledId { get; set; }
+
+    public int BattleId { get; set; }
+    public virtual BattleResult BattleResult { get; set; }
+}
+
+public class BattleResult
+{
+    public int BattleResultId { get; set; }
+    public int WinnerId { get; set; }
+    public int LoserId { get; set; }
+
+    public virtual List<Gladiator> Gladiators { get; set; }
+    public virtual List<GladiatorKills> Killed { get; set; }
+
+}
+
+public class EditorContext : DbContext
+{
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=GladiatorDB;");
+    }
+
+    public DbSet<User> Users {get;set;}
+    public DbSet<Lanista> Lanistas { get; set; }
+    public DbSet<Gladiator> Gladiators { get; set; }
+    public DbSet<Item> Items { get; set; }
+    public DbSet<BattleResult> BattleResults { get; set; }
+    public DbSet<GladiatorKills> GladiatorKills { get; set; }
+
+}
