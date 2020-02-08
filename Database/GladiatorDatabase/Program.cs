0a7352520aa30644bda519a36a7068aad27d6488
rekcos <kraftkon@hotmail.com>
2020-02-08 23:52:34 +0100

    NÃtt

--- Database/GladiatorDatabase/Program.cs
+++ Database/GladiatorDatabase/Program.cs
@@ -61,7 +61,7 @@ namespace GladiatorDatabase
             }
         }
     }
-}
+
 
 public class Usermanager
 {
@@ -76,7 +76,7 @@ public class Usermanager
     {
         Console.WriteLine("\n*Create User*");
         var name = userName;
-        var User = new User { UserName = name, Password = password, Email = email };
+        var User = new User { UserName = name, Password = password, Email = email, IsAdmin = false };
         var dupUser = DB.Users.Where(s => s.UserName == name).ToList();
         var colit = dupUser.Count;
         if (colit != 0)
@@ -89,10 +89,11 @@ public class Usermanager
             DB.SaveChanges();
         }
     }
-    public void Read()
+    public List<User> ReadAllUser()
     {   
-        Console.WriteLine("\n*Read User*");
-        var SortUsers = DB.Users.OrderBy(s => s.UserName);
+        Console.WriteLine("\n*Read AllUser*");
+        var SortUsers = DB.Users.OrderBy(s => s.UserName).
+        return SortUsers;
         var check = SortUsers.Count<User>();
         if (check > 0)
         {
@@ -101,17 +102,18 @@ public class Usermanager
                 Console.WriteLine("User: {0} UserId: {1}", item.UserName, item.UserId);
             }
         }
-        else 
-        { 
-            Console.WriteLine("No Users!"); 
+        else
+        {
+            Console.WriteLine("No Users!");
         }
     }
-    public void Update(User user, string userName, string password, string email)
+    public void Update(User user, string userName, string password, string email, bool admin)
     {
         Console.WriteLine("\n*Update User*");
         user.UserName = userName;
         user.Password = password;
         user.Email = email;
+        user.IsAdmin = admin;
 
         Console.WriteLine("Confirm [y]: ");
         var key = Console.ReadKey();
@@ -137,6 +139,132 @@ public class Usermanager
     }
 
 }
+public class Lanistamanager
+{
+    public Lanistamanager(EditorContext db)
+    {
+        DB = db;
+    }
+
+    EditorContext DB { get; set; }
+
+    public void Create(string lanistaName, int userId)
+    {
+        Console.WriteLine("\n*Create Lanista*");
+        var name = lanistaName;
+        var Lanista = new Lanista { LanistaName = name, UserId = userId, Money = 100 };
+        DB.Lanistas.Add(Lanista);
+        DB.SaveChanges();
+    }
+    public void Read()
+    {
+        /*todo change all *manger read*/
+        Console.WriteLine("\n*Read Lanista*");
+        var SortUsers = DB.Users.OrderBy(s => s.UserName);
+        var check = SortUsers.Count<User>();
+        if (check > 0)
+        {
+            foreach (var item in SortUsers)
+            {
+                Console.WriteLine("Lanista: {0} LanistaId: {1}", item.UserName, item.UserId);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No Users!");
+        }
+    }
+    public void Update(Lanista lanista, string lanistaName, Gladiator gladiator)
+    {
+        Console.WriteLine("\n*Update Lanista*");
+        lanista.LanistaName = lanistaName;
+        
+        Console.WriteLine("Confirm [y]: ");
+        var key = Console.ReadKey();
+        if (key.Key == ConsoleKey.Y)
+        {
+            DB.Lanistas.Update(lanista);
+            DB.SaveChanges();
+        }
+    }
+    public void Destroy(Lanista lanista)
+    {
+        Console.WriteLine("\n*Destroy Lanista*");
+        DB.Lanistas.Remove(lanista);
+        
+        Console.WriteLine("Confirm Delete [y]: ");
+        var key = Console.ReadKey();
+        if (key.Key == ConsoleKey.Y)
+        {
+            DB.SaveChanges();
+        }
+
+    }
+
+}
+public class Gladiatormanager
+{
+    public Gladiatormanager(EditorContext db)
+    {
+        DB = db;
+    }
+
+    EditorContext DB { get; set; }
+
+    public void Create(Shop shop)
+    {
+        Console.WriteLine("\n*Create Gladiator*");
+        var owner = shop.ShopId;
+        var Gladiator = new Gladiator { ShopId = owner };
+        DB.Lanistas.Add(Lanista);
+        DB.SaveChanges();
+    }
+    public void Read()
+    {
+        /*todo change all *manger read*/
+        Console.WriteLine("\n*Read Lanista*");
+        var SortUsers = DB.Users.OrderBy(s => s.UserName);
+        var check = SortUsers.Count<User>();
+        if (check > 0)
+        {
+            foreach (var item in SortUsers)
+            {
+                Console.WriteLine("Lanista: {0} LanistaId: {1}", item.UserName, item.UserId);
+            }
+        }
+        else
+        {
+            Console.WriteLine("No Users!");
+        }
+    }
+    public void Update(Lanista lanista, string lanistaName)
+    {
+        Console.WriteLine("\n*Update Lanista*");
+        lanista.LanistaName = lanistaName;
+
+        Console.WriteLine("Confirm [y]: ");
+        var key = Console.ReadKey();
+        if (key.Key == ConsoleKey.Y)
+        {
+            DB.Lanistas.Update(lanista);
+            DB.SaveChanges();
+        }
+    }
+    public void Destroy(Lanista lanista)
+    {
+        Console.WriteLine("\n*Destroy Lanista*");
+        DB.Lanistas.Remove(lanista);
+
+        Console.WriteLine("Confirm Delete [y]: ");
+        var key = Console.ReadKey();
+        if (key.Key == ConsoleKey.Y)
+        {
+            DB.SaveChanges();
+        }
+
+    }
+
+}
 
 public class User
 {
@@ -144,6 +272,7 @@ public class User
     public string UserName { get; set; }
     public string Password { get; set; }
     public string Email { get; set; }
+    public bool IsAdmin { get; set; }
     public virtual List<Lanista> Lanistas { get; set; }
      
 }
@@ -152,6 +281,7 @@ public class Lanista
 {
     public int LanistaId { get; set; }
     public string LanistaName { get; set; }
+    public int Money { get; set; }
 
     public int UserId { get; set; }
     public virtual User User { get; set; }
@@ -176,6 +306,8 @@ public class Gladiator
     public int Kills { get; set; }
     public bool Alive { get; set; }
 
+    public virtual int ShopId { get; set; }
+    public virtual Shop Shop { get; set; }
     public int LanistaId { get; set; }
     public virtual Lanista Lanista { get; set; }
     public int BattleId { get; set; }
@@ -196,7 +328,8 @@ public class Item
     public int Morale;
     public int Strength;
     public int Defense;
-    
+
+    public virtual Shop Shop { get; set; }
     public virtual Lanista Lanista { get; set; }
     public virtual Gladiator Gladiator { get; set; }
      
@@ -223,6 +356,14 @@ public class BattleResult
 
 }
 
+public class Shop
+{
+    public int ShopId { get; set; }
+
+    public virtual List<Gladiator> Gladiators { get; set; }
+    public virtual List<Item> Items { get; set; }
+}
+
 public class EditorContext : DbContext
 {
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -236,5 +377,6 @@ public class EditorContext : DbContext
     public DbSet<Item> Items { get; set; }
     public DbSet<BattleResult> BattleResults { get; set; }
     public DbSet<GladiatorKills> GladiatorKills { get; set; }
-
+    public DbSet<Shop> Shop { get; set; }
+}
 }
