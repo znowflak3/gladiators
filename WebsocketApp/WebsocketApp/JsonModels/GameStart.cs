b6342e8a89588e8e801e02877de9da5ddf413583
znowflak3 <joakim.znoflinga@yahoo.com>
2020-02-08 22:07:14 +0100

    fixed some stuff in gamemanager

---
+++ WebsocketApp/WebsocketApp/JsonModels/GameStart.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using WebsocketApp.Battle;
+
+namespace WebsocketApp.JsonModels
+{
+    public class GameStart
+    {
+        public string PlayerName { get; set; }
+        public string PlayerHealth { get; set; }
+        public List<string> PlayerSkills { get; set; }
+        public string EnemyName { get; set; }
+        public string EnemyHealth { get; set; }
+        public string Turn { get; set; }
+        
+    }
+}
